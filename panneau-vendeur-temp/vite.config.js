ae0bad133589f1c313e346b320c367b987cb5733
BeautyBook <appsbeautybook@gmail.com>
2026-07-20 01:10:35 +0200

    fix: créer vite.config.js pour panneau vendeur avec alias @

---
+++ panneau-vendeur-temp/vite.config.js
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 5175,
+    open: '/'
+  },
+  plugins: [
+    react(),
+  ]
+});
